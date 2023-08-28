bb298aa05c56425084e52dfea2873b274f53c249
Srivani <SrivaniChennupati@gmail.com>
2023-08-28 13:19:50 -0500

    Web_front_end

--- web_front_end.sh
+++ web_front_end.sh
@@ -13,11 +13,11 @@ Y="\e[33m"
 validate() {
 if [ $1 -ne 0 ]
 then
-    echo "$2.......$R FAILURE $N"
+    echo -e "$2.......$R FAILURE $N"
     exit 1 
 else 
 
-    echo "$2.......$G SUCCESS $N"
+    echo  -e "$2.......$G SUCCESS $N"
 fi    
 }
 
@@ -25,7 +25,7 @@ user_id=$(id -u)
 
 if [ $user_id -ne 0 ] 
 then
-    echo "$R ERROR : This command has to run with the Root Access $N"
+    echo -e "$R ERROR : This command has to run with the Root Access $N"
     exit 1 
 
 fi 
@@ -44,7 +44,7 @@ validate $? "NGINX Started"
 
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>$Log_file
 
 validate $? "Artifact Download"
 
