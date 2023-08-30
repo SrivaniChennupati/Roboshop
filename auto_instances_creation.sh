e8f9266bab90c961c747f9d22a66d63eed5fc411
Srivani <SrivaniChennupati@gmail.com>
2023-08-29 21:36:42 -0500

    payment

--- auto_instances_creation.sh
+++ auto_instances_creation.sh
@@ -4,7 +4,7 @@ Instances=("Web" "Mongodb" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" 
 
 Image_id=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
-Instance_Type="t2.micro"
+Instance_Type=""
 
 for i in "${Instances[@]}"
 do 
@@ -22,6 +22,11 @@ do
 
 echo "Creating Instance : $i"
 aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+
+
+
+
+
 
 done
 
