1620d741fe9daba61e589a375f5c41cc744ed2a2
Srivani <SrivaniChennupati@gmail.com>
2023-08-29 22:01:20 -0500

    automating the instances

--- auto_instances_creation.sh
+++ auto_instances_creation.sh
@@ -5,28 +5,45 @@ Instances=("Web" "Mongodb" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" 
 Image_id=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
 Instance_Type=""
+domain_name=devopsvani.online
 
 for i in "${Instances[@]}"
 do 
  #echo "$i"
- Instance_Type="t2.micro"
+ #Instance_Type="t2.micro"
 
  if [[ $i == "Mongodb" || $i == "MySQL" ]]
-
  then
     $Instance_Type="t3.micro"
  else 
-
     $Instance_Type="t2.micro"
  fi   
 
 echo "Creating Instance : $i"
-aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+private_ip=aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress'
 
+ echo "created $i Instance : $private_ip"
 
+ aws route53 change-resource-record-sets --hosted-zone-id Z00901702AI0X0PSLUZQF --change-batch '
 
-
-
-
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "'$i.$domain_name'.com",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "'$private_ip'"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
 done
 
