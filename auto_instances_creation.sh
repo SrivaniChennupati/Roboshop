5891bd00e307d3ab9db6baf0e6b52588f4bd9a9b
Srivani <SrivaniChennupati@gmail.com>
2023-08-29 20:20:05 -0500

    Automating Instances Creation

--- auto_instances_creation.sh
+++ auto_instances_creation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-Instances=("Web","Mongodb","Catalogue","Redis","User","Cart","MySQL","Shipping","RabbitMQ","Payments","Dispatch")
+Instances=("Web" "Mongodb" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" "RabbitMQ" "Payments" "Dispatch")
 
 for i in "${Instances[@]}"
 do 
- echo "$i \n"
+ echo "$i"
 
 done
 
