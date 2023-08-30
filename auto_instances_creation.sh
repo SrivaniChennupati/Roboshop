370668ed6f009e732147ddaf7816edd0eb182cad
Srivani <SrivaniChennupati@gmail.com>
2023-08-29 22:31:08 -0500

    automating the instances

--- auto_instances_creation.sh
+++ auto_instances_creation.sh
@@ -6,6 +6,7 @@ Image_id=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
 INSTANCE_TYPE=""
 domain_name=devopsvani.online
+Hostedzone_id=Z00901702AI0X0PSLUZQF
 
 for i in "${Instances[@]}"
 do 
@@ -24,24 +25,16 @@ private_ip=$(aws ec2 run-instances --image-id $Image_id --instance-type $INSTANC
 
  echo "created $i Instance :$private_ip"
 
- aws route53 change-resource-record-sets --hosted-zone-id Z00901702AI0X0PSLUZQF --change-batch '{
-  "Comment": "CREATE/DELETE/UPSERT a record ",
-  "Changes": [
-    {
-      "Action": "CREATE",
-      "ResourceRecordSet": {
-        "Name": "'$i.$domain_name'.com",
-        "Type": "A",
-        "TTL": 300,
-        "ResourceRecords": [
-          {
-            "Value": "'$private_ip'"
-          }
-        ]
-      }
+ aws route53 change-resource-record-sets --hosted-zone-id $Hostedzone_id --change-batch '{
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$private_ip'"}]
+                        }}]
     }
-  ]
-}
 '
 done
 
