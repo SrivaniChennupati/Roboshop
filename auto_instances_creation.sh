9559f0e1360329630acfc71389417d071f583aea
Srivani <SrivaniChennupati@gmail.com>
2023-08-29 21:18:31 -0500

    payment

--- auto_instances_creation.sh
+++ auto_instances_creation.sh
@@ -4,7 +4,7 @@ Instances=("Web" "Mongodb" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" 
 
 Image_id=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
-Instance_Type=""
+Instance_Type="t2.micro"
 
 for i in "${Instances[@]}"
 do 
@@ -19,7 +19,7 @@ do
  fi   
 
 echo "Creating Instance : $i"
-aws ec2 run-instances --image-id $Image_id --instance-type "t2.micro" --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
 
 done
 
